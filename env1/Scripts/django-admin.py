882b4092cd54a8264658d57a5e63de1489c9ea49
Kh Azizur Rahman <azizur77@yahoo.com>
2013-10-16 20:35:41 +0200

    Added readme

---
+++ env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\djangoprac\env1\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'django==1.5.4','django-admin.py'
+__requires__ = 'django==1.5.4'
+import pkg_resources
+pkg_resources.run_script('django==1.5.4', 'django-admin.py')
